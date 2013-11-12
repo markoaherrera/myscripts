c121f2b8008f93032c4befa5fa77a5cc376f9e16
markoaherrera <markoaherrera@gmail.com>
2013-11-11 22:42:46 -0600

    Nuevo script que busca en la rae y muestra resultado en lynx

---
+++ raelynx.py
@@ -0,0 +1,12 @@
+""" 
+Muestra resultado de busqueda en lynx
+"""
+import sys
+import webbrowser as wb
+import urllib
+
+lk = wb.get('lynx')
+termino = sys.argv[1]
+url = 'http://lema.rae.es/drae/srv/search?val=' + urllib.quote_plus(termino)
+lk.open(url)
+
