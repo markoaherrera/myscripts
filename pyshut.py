35616be11d5b253f6b5997702a2e13205fe2d467
markoaherrera <markoaherrera@gmail.com>
2014-04-03 17:42:46 -0600

    aceptar parametros

--- pyshut.py
+++ pyshut.py
@@ -1,11 +1,24 @@
 import os
 import time
 
-t = 1
-while (t < 10):
-	print "Minuto {}".format(t)
-	time.sleep(60)
-	t = t + 1
 
-#os.popen("sudo poweroff", "w").write("password")
-print "Ejecucion poweroff"
+def temporizar(tiempo):
+	t = 1
+	while (t < tiempo):
+		print "Minuto {}".format(t)
+		time.sleep(60)
+		t = t + 1
+
+def apagar():
+	print "Ejecucion poweroff"
+
+if __name__ == "__main__":
+	mins = sys.argv[1]
+	mensaje = "Advertencia el sistema se apagará en {} minutos"
+	if mins is not None and mins > 0:
+		print mensaje.format(mins)
+		temporizar(mins)
+	else:
+		print mensaje.format(10)
+		temporizar(10)
+	apagar()
